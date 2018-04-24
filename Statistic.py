32c6ecb258130daa084df182e350988b7180ee34
fancyqlx <fancyqlx@163.com>
2018-04-24 09:25:24 +0800

    new picture

--- Statistic.py
+++ Statistic.py
@@ -49,6 +49,8 @@ def travelGirthFiles():
     ynr = [] # y-axis of r when x-axis is n
     ywp = [] # y-axis of p when x-axis is w
     ywr = [] # y-axis of r when x-axis is w
+    xng = [] # x-axis of g
+    xwg = []
     files = os.listdir(dirPath)
     N = 100
     M = int(1.2*N)
@@ -61,6 +63,7 @@ def travelGirthFiles():
                 with open(dirPath+"/"+filename,"r") as f:
                     n,m,g,p,r = averageGirth(f)
                     xn.append(n)
+                    xng.append(g)
                     ynp.append(p)
                     ynr.append(r)
         N += 100
@@ -78,10 +81,11 @@ def travelGirthFiles():
                 with open(dirPath+"/"+filename,"r") as f:
                     n,m,g,p,r = averageGirth(f)
                     xw.append(W)
+                    xwg.append(g)
                     ywp.append(p)
                     ywr.append(r)
         W += 10
-    return xn,xw,ynp,ynr,ywp,ywr
+    return xn,xw,ynp,ynr,ywp,ywr,xng,xwg
 
 
 def travelFiles(algName):
@@ -145,7 +149,7 @@ def drawFic(gx,gy,bx,by,tx,ty,labelx,labely,filename):
 if __name__ == "__main__":
     BellmanFord = "BellmanFord"
     TrivalBFS = "TrivalBFS"
-    g_xn,g_xw,g_ynp,g_ynr,g_ywp,g_ywr = travelGirthFiles()
+    g_xn,g_xw,g_ynp,g_ynr,g_ywp,g_ywr,x_ng,x_wg= travelGirthFiles()
     b_xn,b_xw,b_ynr,b_ywr = travelFiles(BellmanFord)
     t_xn,t_xw,t_ynr,t_ywr = travelFiles(TrivalBFS)
     # four pictures for girth
@@ -153,6 +157,20 @@ if __name__ == "__main__":
     drawFicGirth(g_xn,g_ynp,"n","the number of phases","girth_n_p")
     drawFicGirth(g_xw,g_ywr,"w","round compleixty","girth_w_r")
     drawFicGirth(g_xw,g_ywp,"w","the number of phases","girth_w_p")
+    drawFicGirth(g_xn,x_ng,"n","g","girth_n_g")
+    drawFicGirth(g_xw,x_wg,"w","g","girth_w_g")
+
+    # draw the curve of g and p
+    dict_g_p = {}
+    for i in xrange(0,len(x_ng)):
+        dict_g_p[x_ng[i]] = g_ynp[i]
+    x = sorted(dict_g_p.keys())
+    y = []
+    for k in x:
+        y.append(dict_g_p[k])
+
+    drawFicGirth(x,y,"g","the number of phases","girth_g_p")
+
     # two pictures for comparision
     drawFic(g_xn,g_ynr,b_xn,b_ynr,t_xn,t_ynr,"n","round complexity","cmp_n_r")
     drawFic(g_xw,g_ywr,b_xw,b_ywr,t_xw,t_ywr,"w","round complexity","cmp_w_r")
